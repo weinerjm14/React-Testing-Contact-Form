55e97a3ee648a7c700cc30fb7accda16a6acb434
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-04-17 13:00:45 -0400

    fail with undefined on first name-- due to maxLength

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
+import { act } from "react-dom/test-utils";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("renders form", () => {
+  render(<ContactForm />);
 });
 
 test("shows labels", () => {
@@ -19,10 +25,17 @@ test("shows labels", () => {
   expect(textBoxLabel).toBeVisible();
 });
 
-test("buttons exist and work", () => {
-  const { getByTestId } = render(<App />);
+test("button exist and work", () => {
+  const { getByTestId, getByText } = render(<ContactForm />);
   const button = getByTestId("button");
-  // fireEvent.click(button)
-
+  const firstNameLabel = getByText(/first name/i);
+  const lastNameLabel = getByText(/last name/i);
+  const emailLabel = getByText(/email/i);
+  const textBoxLabel = getByText(/message/i);
+  act(() => {
+    fireEvent.change(firstNameLabel, { value: "Jennifer" });
+  });
+  fireEvent.click(button);
+  expect(firstNameLabel.value).toBe("Jennifer");
   expect(button).toBeVisible();
 });
